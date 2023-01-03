0a0026d6942391d37d182a2f472058f7307cbc44
mona <hiba.al-aani@digitalcareerinstitute.org>
2023-01-03 01:31:14 +0100

    modify cors2

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 // app.use(cors({origin: true, credentials: true}));
-app.use(cors({origin: "https://front-app-p7qd.onrender.com"}));
+var corsOptions = {
+  origin: "http://example.com",
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 /* You need to actually serve the images that reside in your uploads folder. One way to do this is to use the express static middleware. Assuming your uploads folder resides in your app's root, you'd simply add to your express app:
 
